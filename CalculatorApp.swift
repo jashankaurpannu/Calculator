902712570ecb40175e4702e3f632d7169349a0d3
Jashan Kaur Pannu <79289867+jashankaurpannu@users.noreply.github.com>
2022-01-13 18:27:34 -0500

    Add files via upload

---
+++ CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Jashan Kaur on 2021-12-27.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
